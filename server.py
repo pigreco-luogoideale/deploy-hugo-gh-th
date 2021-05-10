443f7906ed385b7771350f8164064c43ff2d5dbb
Alessandro Re <ale@ale-re.net>
2021-05-10 15:34:56 +0200

    Using sync to copy data

--- server.py
+++ server.py
@@ -105,7 +105,7 @@ async def homepage(request):
     # Great, the site was compiled! Now upload it to ftp
     logging.info(f"Running rclone copy -v {rclone_source} {rclone_target}")
     status = subprocess.run(
-        ["rclone", "copy", "-v", rclone_source, rclone_target],
+        ["rclone", "sync", "-v", rclone_source, rclone_target],
         cwd=(repos_dir / repo),
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
